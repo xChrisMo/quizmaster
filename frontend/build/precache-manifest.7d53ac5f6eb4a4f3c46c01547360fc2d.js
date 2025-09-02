b7178d0b1bedbccf1f397bdaabe74decb398beab
chrismo <chrismo@example.com>
2025-09-02 22:50:11 +0100

    Fixing category images nwo

--- frontend/build/precache-manifest.7d53ac5f6eb4a4f3c46c01547360fc2d.js
+++ frontend/build/precache-manifest.7d53ac5f6eb4a4f3c46c01547360fc2d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b5b6208a9fdc830dbfed49670db33354",
+    "revision": "c8b8d7a54378c0067e19e102331d2c7f",
     "url": "/index.html"
   },
   {
-    "revision": "8d6ef9d6a34ae2fa84a2",
+    "revision": "46bdb7eb322d910090a6",
     "url": "/static/css/main.e2270bd8.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.2947e9c0.chunk.js"
   },
   {
-    "revision": "8d6ef9d6a34ae2fa84a2",
-    "url": "/static/js/main.745bc13c.chunk.js"
+    "revision": "46bdb7eb322d910090a6",
+    "url": "/static/js/main.b860fc61.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
